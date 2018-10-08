dc3986d2ab4973ae1d38da13de3818b4bd3eb9e5
juliotwo1996 <juliotwo1996@gmail.com>
2018-10-08 01:18:22 -0500

    dev server

---
+++ webpack-dev-server/index.js
@@ -0,0 +1,5 @@
+import './estilos.css'
+
+
+document.write("hola mundo en directo")
+console.log('Hola mundo del CSS ')
---
+++ webpack-dev-server/webpack.config.js
@@ -0,0 +1,26 @@
+const path=require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname,'index.js'),
+  output: {
+  	path:path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  devServer:{
+    port:9000
+  },
+  module:{
+  	rules:[
+  	//aqui van los loaders
+  		{
+  			// test: que tipo de archivo quiero reconocer
+  			// use: que loader se va encargar del archivo
+  			test: /.\.css$/,
+  			use: ['style-loader','css-loader'],
+  		}
+
+  	]
+
+  }
+}
